553e8be920e082f367eaccec023a1dd0272feff1
Mayur Deshmukh <mayur@localhost.localdomain>
2018-01-03 17:03:40 +0530

    Added dynamic page titles, created a web app manifest file, and some tweaks to the NotFound and UnderConstruction page backgrounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Mayur Deshmukh';
+
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title) { }
+
+  ngOnInit() {
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.activatedRoute)
+      .map((route) => {
+        while (route.firstChild) { route = route.firstChild; }
+        return route;
+      })
+      .filter((route) => route.outlet === 'primary')
+      .mergeMap((route) => route.data)
+      .subscribe((event) => {
+        let title = this.title;
+        if (event['title'] != null) {
+          title = event['title'] + ' | ' + title;
+        }
+        this.titleService.setTitle(title);
+      });
+  }
 }
