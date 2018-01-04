8a62211ad7d8553cbccff91fac06ca4eae91af4f
Mayur Deshmukh <deshmukhmayur@outlook.com>
2018-01-04 20:09:52 +0530

    Added an Avatar on homepage and set the values for skills

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MdcLinearProgress } from '@angular-mdc/web';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('uiProgress') uiProgress: MdcLinearProgress;
+  @ViewChild('frontEndProgress') frontEndProgress: MdcLinearProgress;
+  @ViewChild('backEndProgress') backEndProgress: MdcLinearProgress;
 
   constructor() { }
 
   ngOnInit() {
+    this.uiProgress.indeterminate = false;
+    this.frontEndProgress.indeterminate = false;
+    this.backEndProgress.indeterminate = false;
+
+    this.uiProgress.setProgress(0.6);
+    this.frontEndProgress.setProgress(0.8);
+    this.backEndProgress.setProgress(0.7);
   }
 
 }
